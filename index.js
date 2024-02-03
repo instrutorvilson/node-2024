0acb6c6df05a227634e41c15586531b2a251f3bb
Raí Ernandes da Silva <74011@PROWAY.TREINA>
2024-02-03 09:33:10 -0300

    Rota post habilitar midleware para pegar valores do body

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 const dados = [
     {
         id:1,
@@ -28,6 +30,24 @@ app.get('/contatos/:id', (req, res) => {
     res.send(contato[0])
 })
 
+app.post('/contatos',(req, res)=>{
+   let contato = {
+    id: dados.length+1,
+    nome: req.body.nome,
+    email:req.body.email
+   }
+  dados.push(contato)
+  res.status(201).json(contato)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+/**
+ * get -> contatos
+ * get -> contatos/1
+ * delete -> contatos/1
+ * put -> contatos/1
+ * post -> contatos
+ */
