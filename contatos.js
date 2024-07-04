a648b962473f4148aafb78858e368fdced72f00f
instrutorvilson <instrutorvilsonmoro@gmail.com>
2024-07-04 20:45:47 -0300

    Implementação acessando banco de dados

--- contatos.js
+++ contatos.js
@@ -2,53 +2,85 @@ const express = require('express')
 const router = express.Router()
 const pool = require('./config')
 
-var db = []
-
-router.get('/', async(req, res)=>{
-   try{
+router.get('/', async (req, res) => {
+   try {
       let cliente = await pool.connect()
-      var dados = await cliente.query('select * from tb_contatos')
-      res.send(dados.rows)     
-    }catch(error){
-     res.send(error.message)
-    }   
- })
- 
- router.post('/', async(req, res) => {
-     try {
-         var cliente = await pool.connect()
-        var dados = 
+      let dados = await cliente.query('select * from tb_contatos')
+      cliente.end()
+      res.send(dados.rows)
+   } catch (error) {
+      res.send(error.message)
+   }
+   
+})
+
+router.post('/', async (req, res) => {
+   try {
+      let cliente = await pool.connect()
+      let dados =
          await cliente.query(
-         'insert into tb_contatos(nome, email)values($1,$2) RETURNING *',
-         [req.body.nome, req.body.email])
-         res.status(201).send(dados.rows[0])
-     } catch (error) {
-       res.send(error.message)
-     }
- })
+            'insert into tb_contatos(nome, email)values($1,$2) RETURNING *',
+            [req.body.nome, req.body.email])
+      
+      cliente.end()
 
- router.delete('/:id',(req, res) => {
-    db = db.filter(ct => ct.id != req.params.id)
-    res.status(204).send('')
- })
+      res.status(201).send(dados.rows[0])
+   } catch (error) {
+      res.send(error.message)
+   }
+})
 
- router.get('/:id', (req, res) => {
-    let aux = db.filter(ct => ct.id == req.params.id)
-    res.status(200).send(aux[0])
- })
+router.delete('/:id', async (req, res) => {
+   try {
+      let cliente = await pool.connect()
+      await cliente.query('delete from tb_contatos where id = $1', [req.params.id])
+      cliente.end()
+      res.status(204).send('')      
+   } catch (error) {
+      res.status(404).send(error.message)
+   }
 
- router.put('/:id', (req, res) => {
-    let aux = db.filter(ct => ct.id == req.params.id)
-    if(aux.length > 0){
-       aux[0].nome = req.body.nome  
-       aux[0].email = req.body.email
-       res.status(200).send('Contato alterado com sucesso!')
-    }else{
-        res.status(404).send('Contato não encontrado')
-    }
- })
+})
+
+router.get('/:id', async (req, res) => {
+   try {
+      let cliente = await pool.connect()
+      let dados = await cliente.query('select * from tb_contatos where id = $1',
+       [req.params.id])
+
+       cliente.end()
+      if (dados.rowCount > 0)
+         res.status(200).send(dados.rows[0])
+      else
+         res.status(404).send('Contato não encontrado')
+      
+   } catch (error) {
+      res.status(404).send(error.message)
+   }
+})
+
+router.put('/:id', async(req, res) => {
+   try {
+      let cliente = await pool.connect()
+      let dados = await cliente.query('select * from tb_contatos where id = $1',[req.params.id])
+
+      if(dados.rowCount > 0){
+        let novosValores = [req.body.nome, req.body.email, req.params.id]
+        await cliente.query('update tb_contatos set nome = $1, email = $2 where id = $3', novosValores)
+        
+        cliente.end()
+
+        res.status(200).send('Contato alterado com sucesso')
+      }else{
+         res.status(404).send('Contato não encontrado') 
+      }
+   } catch (error) {
+      res.status(404).send(error.message) 
+   }
+   
+})
 
 
 
- 
- module.exports = router
+
+module.exports = router
