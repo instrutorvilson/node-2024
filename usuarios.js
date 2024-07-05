9ffe41b0f76d90cd487944ef9515b77c43dd0e01
instrutorvilson <instrutorvilsonmoro@gmail.com>
2024-07-05 19:19:02 -0300

    Gerando tokens

--- usuarios.js
+++ usuarios.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const pool = require('./config')
 
 
@@ -25,8 +26,33 @@ router.post('/registrar', async (req, res) => {
     }
 })
 
-router.post('/login', (req, res) => {
-    res.send('login')
+router.post('/login', async (req, res) => {
+    try {
+        let cliente = await pool.connect()
+        let dados = await cliente.query('select * from tb_usuarios where email = $1', [req.body.email])
+        if(dados.rowCount > 0){
+            //comparar a senha        
+           let ok = await bcrypt.compare(req.body.password, dados.rows[0].password)
+           if(ok){
+                //gerar o token
+                let token = jwt.sign({
+                    id: dados.rows[0].id,
+                    nome: dados.rows[0].nome,
+                    email: dados.rows[0].email,
+                    perfil: dados.rows[0].perfil
+                },process.env.SECRET_KEY, { expiresIn: '1h' })
+                res.status(200).send(token)
+            }
+            else
+               res.status(404).send('Senha não confere')
+        }
+        else{
+            res.status(404).send('Usuario não cadastrado')
+        }
+        
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
 })
 
 
